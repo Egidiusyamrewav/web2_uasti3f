5a60b07c922d5127f980bbec5a25125cf737121e
Egidiusyamrewav <egidiusyamrewav@gmail.com>
2023-02-14 15:50:12 +0900

    web2_uasti3f

---
+++ public/content/galeri.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col-12">
+        <h2>Galeri kelas TI3F</h2>
+    </div>
+</div>
+<article>
+               
+                      <img class="foto" src="egi1.jpg" alt="Tidak tersedia gambar" height="200" width="300">
+                      <img class="foto" src="egi2.jpg" alt="Tidak tersedia gambar" height="200" width="300">                   
+                      <img class="foto" src="egi3.jpg" alt="Tidak tersedia gambar" height="200" width="300">
+         </article>
